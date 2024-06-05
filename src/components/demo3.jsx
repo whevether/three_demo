d8431983630936ebd34a0f65f5b033fd84c6226f
whevether <whevether@outlook.com>
2024-06-05 17:56:51 +0800

    update

--- src/components/demo3.jsx
+++ src/components/demo3.jsx
@@ -1,9 +1,14 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Helmet } from 'react-helmet';
 import 'style/demo3.scss';
 const Demo3 = () => {
   const canvasRef = useRef(null);
   const canvasRef1 = useRef(null);
+  const wapperRef = useRef(null);
+  const sizes = {
+    width: 0,
+    height: 0
+  };
   //  这是一个使用redux 封装axios中间件请求示例
   const head = () => {
     return (
@@ -13,10 +18,14 @@ const Demo3 = () => {
       </Helmet>
     );
   };
+  useEffect(()=>{
+   sizes.width =  wapperRef.current.clientWidth;
+   sizes.height = wapperRef.current.clientHeight;
+  },[]);
   return (
     <div className="demo3-body">
       {head()}
-      <div className="demo3-wrapper">
+      <div className="demo3-wrapper" ref={wapperRef}>
         <div className="cursor" />
         <div className="lds-roller"><div /><div /><div /><div /><div /><div /><div /><div /></div>
         <div className="loading-text-intro"><p>Loading</p></div>
